faf9ecb429f4bfde84acc555eee0b17f87bc25ac
DavidBonvin <ledave.musique@gmail.com>
2023-06-15 22:21:11 +0200

    3D ok

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,8 @@
-import { Suspense, useEffect, useState, useRef } from "react";
-import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Suspense, useEffect, useState } from "react";
+
+import { Canvas } from "@react-three/fiber";
 import CanvasLoader from "../Loader";
-
-
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
