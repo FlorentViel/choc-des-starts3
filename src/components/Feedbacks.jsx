1b3ecf7fd74f78086cfdb18b2ce15f0477ed2b4e
DavidBonvin <ledave.musique@gmail.com>
2023-06-19 00:36:46 +0200

    secction projects ok 1.1 sans techos

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+
 
 const Feedbacks = () => {
   return (
