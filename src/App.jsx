76408a9cfbb8649464d8dde8c49e9fcff8802c22
DavidBonvin <ledave.musique@gmail.com>
2023-06-27 12:19:54 +0200

    emails ok 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
           <Works />
           <Feedbacks />
           <div className="relative z-0">
-            {/* <Contact /> */}
+            <Contact />
             <StarsCanvas />
           </div>
         </div>
